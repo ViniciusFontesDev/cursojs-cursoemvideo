e0b97dc4b465ab98900279a5404ac9268186db67
Vinícius Fontes <programadorviniciusfontes@gmail.com>
2024-07-15 15:35:58 -0400

    estudei while, do while e for (estruturads de repetição)

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+/*DO - WHILE*/
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 5)
+
+/*
+WHILE
+var c = 1
+while (c < 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+
+/*
+NORMAL
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,31 @@
+console.log('Vai começar...')
+for(var c = 1; c<=3; c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+//  IGUAIS
+//-----------------------
+/*
+for( c=1; c<=10; c++){
+    console.log(c)
+}
+*/
+//--------------------------
+/*
+var c = 1
+while(c<=5){
+    console.log(c)
+    c++
+}
+*/
+//-------------------------
+/*
+do {
+    console.log(c)
+    c++
+}while(c<=5)
+*/
