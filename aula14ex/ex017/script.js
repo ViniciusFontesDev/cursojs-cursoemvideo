3cee6b25a7283558d0aaca6411654cb2a03e62fd
Vinícius Fontes <programadorviniciusfontes@gmail.com>
2024-07-16 14:35:13 -0400

    2 tentativa

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,11 @@
+function gerarTabuada(){
+    var n = window.document.getElementById('numero')
+    var n = Number(n.value)
+    var res = window.document.getElementById('res')
+
+    for(c = 0; c <= 10; c++)
+        res.innerHTML = `${c}`
+        res.innerHTML = `${c}`
+        res.innerHTML = `${c}`
+        res.innerHTML = `${c}`
+}
